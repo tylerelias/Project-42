b39990b1b05470405390d437f2baeda37e482236
Tyler Jones <tylereliasj@gmail.com>
2021-03-08 23:49:47 +0000

    Setting up a local MongoDB connection

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const helmet = require('helmet');
 const morgan = require('morgan');
 const express = require('express');
+const connect = require('./db/dbConnect');
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
+
+connect.connectDb();
 
 if (app.get('env') === 'development') {
     console.log('Logging enabled');
