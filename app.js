60475d0e33f2aac27b1fd65216803fe3ea5734a4
Tyler Jones <tylereliasj@gmail.com>
2021-03-09 00:04:03 +0000

    Adding Pug views and routes for / and /users

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const express = require('express');
 const connect = require('./db/dbConnect');
-
+// routes
+const home = require('./routes/home');
+const users = require('./routes/users');
+// app
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(express.json());
 app.use(helmet());
 
 connect.connectDb();
+
+app.use('/', home)
+app.use('/api/users', users);
 
 if (app.get('env') === 'development') {
     console.log('Logging enabled');
