5a55efa1b27ed24ac67a93fb4da323ba5d798716
Tyler Jones <tylereliasj@gmail.com>
2021-03-10 13:51:40 +0000

    Renamed files for consistency

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const dbUser = require('../db/dbUser');
+const dbUser = require('../db/dbUsers');
 
 const validateUser = require('../validation/validateUser');
 
