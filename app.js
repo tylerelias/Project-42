89131fe6b051ca630bda0df9d599e823842e6ed7
Tyler Jones <tylereliasj@gmail.com>
2021-03-11 23:12:26 +0000

    export server to use for tests changed 'test' command in package.json

--- app.js
+++ app.js
@@ -13,4 +13,6 @@ require('./startup/config')();
 require('./startup/views')(app);
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`))
+
+module.exports = server;
