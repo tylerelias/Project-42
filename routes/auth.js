949eed4483458111a23e2ffda8f25e1aec655d28
Tyler Jones <tylereliasj@gmail.com>
2021-03-12 09:13:00 +0000

    Added a response where the token is placed in the header Comments added

--- routes/auth.js
+++ routes/auth.js
@@ -4,23 +4,25 @@ const {User} = require('../models/user');
 const Joi = require("joi");
 const bcrypt = require("bcrypt");
 
-router.get('/', async (req, res) => {
-    res.send(true)
-});
-
+// /api/auth/ - user authentication
+// verifies the login credentials and if they are valid
+// a response with an auth-token is given for a log in session
 router.post('/', async (req, res) => {
     const {error} = validate(req.body)
     if (error) return res.status(400)
         .send(error.details[0].message);
 
-    let user = await User.findOne({ email: req.body.email});
+    const user = await User.findOne({ email: req.body.email});
     if (!user) return res.status(400).send('Invalid email or password');
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
 
     const token = user.generateAuthToken();
-    res.send(token);
+
+    res
+        .header('x-auth-token', token)
+        .send(token);
 });
 
 function validate(req) {
