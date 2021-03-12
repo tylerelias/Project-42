c0d119ee140be4aa0ef213f4d045c677126769c2
Tyler Jones <tylereliasj@gmail.com>
2021-03-12 09:36:03 +0000

    code reformatting

--- tests/unit/integration/nations.test.js
+++ tests/unit/integration/nations.test.js
@@ -3,27 +3,26 @@ const mongoose = require('mongoose');
 const {Nation} = require('../../../models/nation')
 const {User} = require('../../../models/user')
 
-
 let server;
 const genericNations = [{
-        name: "GenericName1",
-        population: 1,
-        balance: 1,
-        social_policies: { equality: 0.5, religion: 0.5 },
-        economic_policies: {
-            education: 0.5,
-            healthcare: 0.5,
-            welfare: 0.5,
-            transportation: 0.5,
-            taxation: 0.5
-        },
-        owner: new mongoose.Types.ObjectId()
+    name: "GenericName1",
+    population: 1,
+    balance: 1,
+    social_policies: {equality: 0.5, religion: 0.5},
+    economic_policies: {
+        education: 0.5,
+        healthcare: 0.5,
+        welfare: 0.5,
+        transportation: 0.5,
+        taxation: 0.5
     },
+    owner: new mongoose.Types.ObjectId()
+},
     {
         name: "GenericName2",
         population: 2,
         balance: 2,
-        social_policies: { equality: 0.2, religion: 0.2 },
+        social_policies: {equality: 0.2, religion: 0.2},
         economic_policies: {
             education: 0.2,
             healthcare: 0.2,
@@ -32,15 +31,17 @@ const genericNations = [{
             taxation: 0.2
         },
         owner: new mongoose.Types.ObjectId()
-}]
+    }]
 
-describe('/api/nations', () =>{
-    beforeEach(() => { server = require('../../../app'); })
+describe('/api/nations', () => {
+    beforeEach(() => {
+        server = require('../../../app');
+    })
     afterEach(async () => {
         server.close();
         await Nation.remove({});
     })
-    
+
     describe('GET /', () => {
         it('should return all nations', async () => {
             await Nation.collection.insertMany(genericNations);
@@ -85,7 +86,7 @@ describe('/api/nations', () =>{
             const res = await request(server)
                 .post('/api/nations/')
                 .set('x-auth-token', token)
-                .send({ name: 1});
+                .send({name: 1});
 
             expect(res.status).toBe(400);
         });
@@ -97,7 +98,7 @@ describe('/api/nations', () =>{
             const res = await request(server)
                 .post('/api/nations/')
                 .set('x-auth-token', token)
-                .send({ name: name});
+                .send({name: name});
 
             expect(res.status).toBe(400);
         });
